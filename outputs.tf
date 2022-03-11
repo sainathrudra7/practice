2585277d165d466178ed9dffea5bc36593792204
Mohammed Riyaz <94772181+Riyaz1994@users.noreply.github.com>
2022-03-11 21:38:54 +0530

    updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -25,3 +25,7 @@ output "private_subnet1" {
 output "private_subnet2" {
   value = "${element(aws_subnet.private_subnet.*.id, 2 )}"
 }
+
+output "transit_gateway" {
+  value = "${aws_ec2_transit_gateway.my-test-tgw.id}"
+}
